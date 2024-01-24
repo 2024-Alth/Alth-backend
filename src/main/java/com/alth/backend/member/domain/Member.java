d66a8ab5535383c348db3c64f00e22216bad982f
정우혁 <alalalalalalall367@gmail.com>
2024-01-24 20:28:54 +0900

    feat: Member entity 생성

---
+++ src/main/java/com/alth/backend/member/domain/Member.java
@@ -0,0 +1,38 @@
+package com.alth.backend.member.domain;
+
+import com.alth.backend.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@SQLDelete(sql = "UPDATE member SET is_deleted = true WHERE user_id =?")
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private int birth;
+
+    private Boolean enjoyDrink;
+
+    private String favorLiquor;
+
+    private Boolean isDeleted;
+}
