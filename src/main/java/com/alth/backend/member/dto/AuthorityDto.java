a78b05feecd764fe63e417e79a2bd0c66fab6bef
정우혁 <alalalalalalall367@gmail.com>
2024-01-25 20:04:38 +0900

    feat: 로그인에 필요한 토큰 관련 DTO 설계

---
+++ src/main/java/com/alth/backend/member/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package com.alth.backend.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
---
+++ src/main/java/com/alth/backend/member/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package com.alth.backend.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+    private String token;
+}
