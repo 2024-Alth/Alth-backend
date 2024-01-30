acf2f5edb22ab021bdd60d57aaf3173133502b68
uqualid <uqualid@tukorea.ac.kr>
2024-01-30 18:43:17 +0900

    Changes

---
+++ src/main/java/com/alth/backend/record/domain/Record.java
@@ -0,0 +1,51 @@
+package com.alth.backend.record.domain;
+
+
+import com.alth.backend.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE record SET is_deleted = true WHERE record_id = ?")
+@Where(clause = "is_deleted = false")
+@ToString
+public class Record extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long recordId;
+
+    private int alCnt;
+
+    @Enumerated(EnumType.STRING)
+    private Feels hangOver;
+
+    @Column(length = 500, nullable = false)
+    private String recordMemo;
+
+    private boolean isDeleted = Boolean.FALSE; // default - FALSE
+
+    public enum Feels {
+        ALIVE,
+        LITTLE,
+        FULLY,
+        DEATH; //sat as 4 type(changeable)
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+    }
+
+    public void updateRecord(int alCnt, Feels hangOver, String recordMemo){
+        this.alCnt = alCnt;
+        this.hangOver = hangOver;
+        this.recordMemo = recordMemo;
+    }
+}
