b2c84ffa7ee731cdf63087f7316458c673e37103
uqualid <uqualid@tukorea.ac.kr>
2024-01-30 20:11:27 +0900

    feat: RecordRepository 설계

---
+++ src/main/java/com/alth/backend/record/repository/RecordRepository.java
@@ -0,0 +1,13 @@
+package com.alth.backend.record.repository;
+
+import com.alth.backend.record.domain.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {
+    Record save(Record record);
+    List<Record> findAll();
+    Optional<Record> findById(Long id);
+}
