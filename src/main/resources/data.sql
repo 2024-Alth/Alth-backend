90f93ad0eb52218a21516009c18839068ca02677
uqualid <uqualid@tukorea.ac.kr>
2024-02-01 11:19:18 +0900

    feat: 서버 실행 시 Alcohol 초기 데이터 삽입 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,26 @@
 insert into authority (authority_name)values ('ROLE_USER');
 insert into authority (authority_name)values ('ROLE_ADMIN');
+
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (1, '참이슬 오리지널(빨뚜)', 20.1, 1900, 360, 'SOJU');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (2, '참이슬 후레쉬', 16.5, 1900, 360, 'SOJU');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (3, '과일 소주', 13.0, 2000, 360, 'SOJU');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (4, '매화수', 14.0, 2600, 300, 'SOJU' );
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (5, '별빛 청하', 7.0, 2750, 295, 'SOJU');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (6, '프리미엄 소주', 25.0, 13000, 375, 'SOJU');
+
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (7, '작은 캔', 4.6, 1580, 250, 'BEER');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (8, '중간 캔', 4.6, 2250, 355, 'BEER');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (9, '큰 캔', 4.6, 2800, 500, 'BEER');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (10, '유리 병', 4.6, 3100, 640, 'BEER');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (11, '페트 - 1L', 4.6, 5400, 1000, 'BEER');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (12, '페트 - 1.6L', 4.6, 7900, 1600, 'BEER');
+
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (13, '장수 막걸리', 6.0, 1600, 750, 'MAKGEOLLI');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (14, '생탁 막걸리', 6.0, 1600, 750, 'MAKGEOLLI');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (15, '가평 잣 막걸리', 6.0, 2300, 750, 'MAKGEOLLI');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (16, '우도 땅콩 막걸리', 6.0, 3900, 750, 'MAKGEOLLI');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (17, '곰표 막걸리', 6.0, 4500, 500, 'MAKGEOLLI');
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (18, '국순당 막걸리', 6.0, 2200, 750, 'MAKGEOLLI');
+
+insert into alcohol (alcohol_id, alcohol_name, degree, price, volume, alcohol_type) values (19, '사용자 지정', 0.0, 0, 0, 'ETC');
+
