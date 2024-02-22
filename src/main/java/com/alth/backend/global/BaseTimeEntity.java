9b75affbd8435778770cb10068dadd60776a85a1
정우혁 <alalalalalalall367@gmail.com>
2024-02-22 19:02:59 +0900

    feat: BaseTimeEntity 내부 시간 생성시 저장되는 시간 형식 수정

--- src/main/java/com/alth/backend/global/BaseTimeEntity.java
+++ src/main/java/com/alth/backend/global/BaseTimeEntity.java
@@ -1,20 +1,40 @@
 package com.alth.backend.global;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 public class BaseTimeEntity {
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        String customLocalDateTimeFormat = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        LocalDateTime parsedCreateAt = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.createdAt = parsedCreateAt;
+    }
+
     @LastModifiedDate
+    @Column(updatable = true)
     private LocalDateTime updatedAt;
+
+
+    @PreUpdate
+    public void onPreUpdate() {
+        String customLocalDateTimeFormat = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        LocalDateTime parsedUpdateAt = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.updatedAt = parsedUpdateAt;
+    }
+
 }
